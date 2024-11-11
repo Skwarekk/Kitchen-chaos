f8638ae1e62d8ef4dcf847cf7fa60317123b6f6b
Skwarek <oskarzonca24122008@gmail.com>
2024-11-11 22:50:31 +0100

    Player visual
    Add visual to player and make player look forward while moving

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float rotateSpeed = 10f;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -25,6 +26,6 @@ public class Player : MonoBehaviour
         inputVector = inputVector.normalized;
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
-        Debug.Log(inputVector);
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
