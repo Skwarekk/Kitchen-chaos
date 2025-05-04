e8149ce02cc78a4aa0a68d8786320b480db30329
SkwarekDev <skwarek.develop@gmail.com>
2025-05-04 16:01:53 +0200

    Player animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -28,12 +29,17 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
-        if(moveDir != Vector3.zero)
+        if(isWalking = moveDir != Vector3.zero)
         {
             float rotateSpeed = 10f;
             transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         }
 
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
