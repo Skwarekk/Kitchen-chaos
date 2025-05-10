e2439fbbc45799ee7fc4ed4d1dd4b7b7496d7818
SkwarekDev <skwarek.develop@gmail.com>
2025-05-10 12:35:27 +0200

    Kitchen object parent

--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -4,8 +4,34 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO KitchenObjectSO;
 
+    private ClearCounter clearCounter;
+
     public KitchenObjectSO GetKitchenObjectSO()
     {
         return KitchenObjectSO;
+    }
+
+    public void SetKitchenCounter(ClearCounter clearCounter)
+    {
+        if(this.clearCounter != null)
+        {
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        this.clearCounter = clearCounter;
+
+        if (clearCounter.HasKitchenObject())
+        {
+            Debug.LogError("Counter already has a KitchenObject!");
+        }
+
+        clearCounter.SetKitchenObject(this);
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter()
+    {
+        return clearCounter;
     }
 }
