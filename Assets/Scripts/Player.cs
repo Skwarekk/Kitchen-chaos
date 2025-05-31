8b5797b3df2e8dba4f29636a1afec1033589f670
SkwarekDev <skwarek.develop@gmail.com>
2025-05-31 17:34:51 +0200

    Game start

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -40,6 +40,11 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAction(object sender, System.EventArgs e)
     {
+        if (!GameManager.Instance.IsGamePlaying())
+        {
+            return;
+        }
+
         if (selectedCounter != null)
         {
             selectedCounter.Interact(this);
@@ -48,6 +53,11 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e)
     {
+        if (!GameManager.Instance.IsGamePlaying())
+        {
+            return;
+        }
+
         if (selectedCounter != null)
         {
             selectedCounter.InteractAlternate(this);
